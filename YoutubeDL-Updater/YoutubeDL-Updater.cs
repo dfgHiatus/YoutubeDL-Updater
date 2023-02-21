97ac81dd13686f5e210c5b54bed7e846b05aa33c
Benjamin Evans <51272212+dfgHiatus@users.noreply.github.com>
2023-02-21 14:24:12 -0600

    Added logic to update youtube-dlp using built ins

--- YoutubeDL-Updater/YoutubeDL-Updater.cs
+++ YoutubeDL-Updater/YoutubeDL-Updater.cs
@@ -1,11 +1,11 @@
-﻿using FrooxEngine;
+﻿using BaseX;
+using FrooxEngine;
 using HarmonyLib;
 using NeosModLoader;
 using System;
+using System.Diagnostics;
 using System.IO;
-using System.Net;
 using System.Security.Cryptography;
-using System.Security.Policy;
 
 namespace YoutubeDLUpdater
 {
@@ -13,7 +13,7 @@ namespace YoutubeDLUpdater
     {
         public override string Name => "YoutubeDL-Updater";
         public override string Author => "dfgHiatus and Frozen";
-        public override string Version => "1.0.1";
+        public override string Version => "1.0.2";
         public override string Link => "https://github.com/dfgHiatus/YoutubeDL-Updater";
 
         private static ModConfiguration _modConfig;
@@ -31,8 +31,6 @@ namespace YoutubeDLUpdater
         [AutoRegisterConfigKey]
         private static ModConfigurationKey<bool> _useDefaultYoutubeDl = new ModConfigurationKey<bool>("use_default_youtubeDL", "Use Neos's default YoutubeDL (requires restart)", () => false);
 
-        private static readonly Url PathSha256 = new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/SHA2-256SUMS");
-        private static readonly Url PathYtDlp = new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe");
         private static readonly string ExecutablePath = Path.Combine(Engine.Current.AppPath, "RuntimeData");
         private static readonly string BackupPath = Path.Combine(ExecutablePath, "YoutubeDL-Backups");
         private const string ExecutableName = "yt-dlp.exe";
@@ -67,59 +65,33 @@ namespace YoutubeDLUpdater
             var currentSha256 = ComputeSha256(Path.Combine(ExecutablePath, ExecutableName));
             if (backupSha256 != currentSha256)
                 File.Copy(Path.Combine(BackupPath, ExecutableName), Path.Combine(ExecutablePath, ExecutableName));
-            // modConfig.Set(useDefaultYoutubeDL, false);
         }
 
         private static void UpdateYoutubeDL()
         {
-            // Download the text file at PATH_SHA_256. This might throw, so we need to wrap it
-            string sha256;
-            try
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo()
             {
-                using (var client = new WebClient())
-                    sha256 = client.DownloadString(PathSha256.Value);
-            }
-            catch (Exception e)
-            {
-                Error("An error occured while downloading SHA256 hashes from yt-dlp's github.");
-                Error(e.Message);
-                return;
-            }
-            if (string.IsNullOrEmpty(sha256))
-            {
-                Error("There were no SHA256 hashes from yt-dlp's github. Aborting update.");
-                return;
-            }
-            // Split the string on newlines
-            var allLines = sha256.Split('\n');
-            // Get the entry that ends with "yt-dlp.exe"
-            var line = Array.Find(allLines, x => x.EndsWith(ExecutableName));
-            // We now have a string that looks like "<SHA256>  yt-dlp.exe", with 2 spaces(?!). To get the hash, we remove the executable name plus 2 spaces
-            var stringHash = line.Remove(line.Length - (ExecutableName.Length + 2));
-            // Compute the SHA256 of yt-dlp.exe in the above directory
-            var computedHash = ComputeSha256(Path.Combine(ExecutablePath, ExecutableName));
-            // If these 2 are equal, then we have the latest build
-            if (stringHash == computedHash)
-            {
-                Msg($"yt-dlp.exe is up to date.");
-                return;
-            }
-            Msg($"Local yt-dlp.exe is {computedHash} but remote is {stringHash}! Downloading the latest version...");
-            // Download the binary file at PATH_YT_DLP. This might throw, so we need to wrap it again
-            try
-            {
-                using (var client = new WebClient())
-                    client.DownloadFile(PathYtDlp.Value, Path.Combine(ExecutablePath, ExecutableName));
-            }
-            catch (Exception e)
-            {
-                Error("An error occured while downloading yt-dlp.exe from github.");
-                Error(e.Message);
-                return;
-            }
+                FileName = "cmd.exe",
+                Arguments = $@"/C {ExecutablePath}/{ExecutableName} -U",
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+            };
+            
+            process.StartInfo = startInfo;
+            process.OutputDataReceived += CopyOutput;
+            process.Start();
+            process.WaitForExit();
+
             Msg($"Download complete! yt-dlp.exe is now up to date.");
         }
-        
+
+        private static void CopyOutput(object sender, DataReceivedEventArgs e)
+        {
+            UniLog.Log(e.Data);
+        }
+
         // https://stackoverflow.com/questions/38474362/get-a-file-sha256-hash-code-and-checksum with corrections
         private static string ComputeSha256(string filePath)
         {
