5b787cb8583391596a87ceed8672bbd89fd507e3
Benjamin Evans <51272212+dfgHiatus@users.noreply.github.com>
2023-03-03 08:29:10 -0600

    Start of cross platform support

---
+++ YoutubeDL-Updater/YoutubeDL-Target.cs
@@ -0,0 +1,64 @@
+﻿using FrooxEngine;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+
+namespace YoutubeDLUpdater
+{
+    internal static class YoutubeDLTarget
+    {
+        internal static readonly Url PathSha256 = new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/SHA2-256SUMS");
+        private static string _linuxYtDlpCache;
+
+        internal static Url PathYtDlp
+        {
+            get
+            {
+                switch (Engine.Current.Platform)
+                {
+                    case Platform.Windows:
+                        return new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe");
+                    case Platform.OSX:
+                        return new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos");
+                    case Platform.Linux:
+                        return new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux");
+                    // return GetLinuxPath("ffmpeg", ref _linuxYtDlpCache);
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        internal static string ExecutableName
+        {
+            get
+            {
+                switch (Engine.Current.Platform)
+                {
+                    case Platform.Windows:
+                        return "yt-dlp.exe";
+                    case Platform.OSX:
+                        return "yt-dlp_macos";
+                    case Platform.Linux:
+                        return ("yt-dlp_linux");
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
+
+        // TODO Check if youtube-dl is installed as a package on non-windows machines?
+        private static string GetLinuxPath(string command, ref string cache)
+        {
+            if (string.IsNullOrWhiteSpace(cache))
+            {
+                var path = Environment.GetEnvironmentVariable("PATH") ?? "";
+                var paths = path.Split(Path.PathSeparator).ToList();
+                var test = paths.Select(i => Path.Combine(i, command)).FirstOrDefault(File.Exists);
+                cache = string.IsNullOrWhiteSpace(test) ? "notfound" : test;
+            }
+            return cache;
+        }
+    }
+}
--- YoutubeDL-Updater/YoutubeDL-Updater.cs
+++ YoutubeDL-Updater/YoutubeDL-Updater.cs
@@ -5,7 +5,6 @@ using System;
 using System.IO;
 using System.Net;
 using System.Security.Cryptography;
-using System.Security.Policy;
 
 namespace YoutubeDLUpdater
 {
@@ -13,7 +12,7 @@ namespace YoutubeDLUpdater
     {
         public override string Name => "YoutubeDL-Updater";
         public override string Author => "dfgHiatus and Frozen";
-        public override string Version => "1.0.1";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/dfgHiatus/YoutubeDL-Updater";
 
         private static ModConfiguration _modConfig;
@@ -31,11 +30,8 @@ namespace YoutubeDLUpdater
         [AutoRegisterConfigKey]
         private static ModConfigurationKey<bool> _useDefaultYoutubeDl = new ModConfigurationKey<bool>("use_default_youtubeDL", "Use Neos's default YoutubeDL (requires restart)", () => false);
 
-        private static readonly Url PathSha256 = new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/SHA2-256SUMS");
-        private static readonly Url PathYtDlp = new Url("https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe");
         private static readonly string ExecutablePath = Path.Combine(Engine.Current.AppPath, "RuntimeData");
         private static readonly string BackupPath = Path.Combine(ExecutablePath, "YoutubeDL-Backups");
-        private const string ExecutableName = "yt-dlp.exe";
         
         public override void OnEngineInit()
         {
@@ -57,17 +53,15 @@ namespace YoutubeDLUpdater
             // See if we already have a backup
             if (Directory.GetFiles(BackupPath).Length > 0) return;
             // If we don't have a backup (first time!), make it
-            File.Copy(Path.Combine(ExecutablePath, ExecutableName), Path.Combine(BackupPath, ExecutableName));
+            File.Copy(Path.Combine(ExecutablePath, YoutubeDLTarget.ExecutableName), Path.Combine(BackupPath, YoutubeDLTarget.ExecutableName));
         }
         
         private static void LoadBackup()
         {
-            // Get the SHA256 of the backup and the current executable
-            var backupSha256 = ComputeSha256(Path.Combine(BackupPath, ExecutableName));
-            var currentSha256 = ComputeSha256(Path.Combine(ExecutablePath, ExecutableName));
+            var backupSha256 = ComputeSha256(Path.Combine(BackupPath, YoutubeDLTarget.ExecutableName));
+            var currentSha256 = ComputeSha256(Path.Combine(ExecutablePath, YoutubeDLTarget.ExecutableName));
             if (backupSha256 != currentSha256)
-                File.Copy(Path.Combine(BackupPath, ExecutableName), Path.Combine(ExecutablePath, ExecutableName));
-            // modConfig.Set(useDefaultYoutubeDL, false);
+                File.Copy(Path.Combine(BackupPath, YoutubeDLTarget.ExecutableName), Path.Combine(ExecutablePath, YoutubeDLTarget.ExecutableName));
         }
 
         private static void UpdateYoutubeDL()
@@ -77,7 +71,7 @@ namespace YoutubeDLUpdater
             try
             {
                 using (var client = new WebClient())
-                    sha256 = client.DownloadString(PathSha256.Value);
+                    sha256 = client.DownloadString(YoutubeDLTarget.PathSha256.Value);
             }
             catch (Exception e)
             {
@@ -92,12 +86,12 @@ namespace YoutubeDLUpdater
             }
             // Split the string on newlines
             var allLines = sha256.Split('\n');
-            // Get the entry that ends with "yt-dlp.exe"
-            var line = Array.Find(allLines, x => x.EndsWith(ExecutableName));
-            // We now have a string that looks like "<SHA256>  yt-dlp.exe", with 2 spaces(?!). To get the hash, we remove the executable name plus 2 spaces
-            var stringHash = line.Remove(line.Length - (ExecutableName.Length + 2));
-            // Compute the SHA256 of yt-dlp.exe in the above directory
-            var computedHash = ComputeSha256(Path.Combine(ExecutablePath, ExecutableName));
+            // Get the entry that ends with our os's "yt-dlp" name
+            var line = Array.Find(allLines, x => x.EndsWith(YoutubeDLTarget.ExecutableName));
+            // We now have a string that looks like "<SHA256>  yt-dlp...", with 2 spaces(?!). To get the hash, we remove the executable name plus 2 spaces
+            var stringHash = line.Remove(line.Length - (YoutubeDLTarget.ExecutableName.Length + 2));
+            // Compute the SHA256 of yt-dlp in the above directory
+            var computedHash = ComputeSha256(Path.Combine(ExecutablePath, YoutubeDLTarget.ExecutableName));
             // If these 2 are equal, then we have the latest build
             if (stringHash == computedHash)
             {
@@ -105,11 +99,11 @@ namespace YoutubeDLUpdater
                 return;
             }
             Msg($"Local yt-dlp.exe is {computedHash} but remote is {stringHash}! Downloading the latest version...");
-            // Download the binary file at PATH_YT_DLP. This might throw, so we need to wrap it again
+            // Download the binary file at YoutubeDLTarget.PathYtDlp. This might throw, so we need to wrap it again
             try
             {
                 using (var client = new WebClient())
-                    client.DownloadFile(PathYtDlp.Value, Path.Combine(ExecutablePath, ExecutableName));
+                    client.DownloadFile(YoutubeDLTarget.PathYtDlp.Value, Path.Combine(ExecutablePath, YoutubeDLTarget.ExecutableName));
             }
             catch (Exception e)
             {
